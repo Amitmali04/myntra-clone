1ff5c81a028f3e658fb39a98e204e2298571d086
Amit <amit@techfra.in>
2023-04-28 07:17:43 +0530

    Myntra clone

---
+++ src/components/Cart/Cart.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+// import { Data } from '../Data';
+
+export function Cart({ Modal }) {
+ 
+
+  return (
+    <div>
+      <h1>bad (internet) connection </h1>
+    </div>
+  )
+}
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,8 @@
+import './Footer.css'
+export function Footer() {
+  return (
+    <div className="footer-wrapper">
+      <p>Desgined and Developed by <span className='footer-span'>@data</span></p>
+    </div>
+  )
+}
